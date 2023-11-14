59b02ad522b6d6ad577e64b3ac2b8582ef896195
Chrisfg <47301396+catgirlchris@users.noreply.github.com>
2023-11-14 12:00:01 +0100

    Initial commit

---
+++ SGE03_2/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "SGE03_2",
+    'version': '15.0.0.1',
+    'depends': ['base', 'project'],
+    'author': "Openred",
+    'category': "Sales",
+    'description': """
+        SGE03_2
+    """,
+    'data':[
+        'views/res_partner_views.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False
+}
